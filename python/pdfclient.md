d22be0910e5a7093e3656a4e229591a35a02ff03
Felix Yen <fyen@datalogics.com>
2013-08-02 14:17:46 -0500

    refactor client

--- python/pdfclient.md
+++ python/pdfclient.md
@@ -1,3 +1,5 @@
+TODO: brief description of API
+
 [Sample Python client for Datalogics PDF API](pdfclient_8py_source.html) 
 
 ### Dependencies
@@ -5,3 +7,7 @@
 * Python (2.7)
 * [Requests](http://docs.python-requests.org/en/latest/): HTTP for Humans (1.2.3)
 * [3scale](http://3scale.net): API Management
+
+### Resources
+
+* [PDF2IMG](../PDF2IMG.pdf): PDF2IMG User Guide
--- python/pdfclient.py
+++ python/pdfclient.py
@@ -1,45 +1,96 @@
 # Copyright (c) 2013, Datalogics, Inc. All rights reserved.
 
+# TODO: sample disclaimer
+
 'Sample pdfprocess client module'
 
 
 import requests
 
 
-class Request(object):
+class Client(object):
+    BASE_URL = 'https://api.pdfprocess.datalogics-cloud.com'
     VERSION = 0
-    URL = 'https://api.pdfprocess.datalogics-cloud.com'
-    ## @brief set #api_key and #url
-    ## @param request_type e.g. 'image'
-    ## @param api_key from <a href="http://3scale.net/">TODO: fix link</a>
-    def __init__(self, request_type, api_key, version=VERSION, base_url=URL):
-        self._api_key = api_key
-        self._data = None
-        self._url = '%s/%s/actions/%s' % (base_url, version, request_type)
 
-    ## @brief set #data and return a requests.Response object
-    ## @param input request document file object
-    ## @param options request options, e.g. {'pages': '1', 'noAnnot': True}
-    def post(self, input, **options):
-        self._data = options.copy()
-        self._data['apiKey'] = self.api_key
-        self._data['inputFile'] = input.name
-        return requests.post(self.url, data=self.data, files={'input': input})
+    ## Set #api_key and #base_url
+    #  @param api_key from <a href="http://3scale.net/">TODO: fix link</a>
+    def __init__(self, api_key, version=VERSION, base_url=BASE_URL):
+        self._api_key = api_key
+        self._base_url = '%s/%s' % (base_url, version)
+
+    ## Request factory
+    # @return a Request object
+    # @param request_type e.g. 'image'
+    def make_request(self, request_type):
+        if request_type == 'image':
+            return ImageRequest(self)
 
     @property
     ## API key property
     def api_key(self): return self._api_key
     @property
-    ## request data property, set by #post
+    ## %Client URL property
+    def base_url(self): return self._base_url
+
+
+class Request(object):
+    def __init__(self, client, request_type):
+        self._data = {'apiKey': client.api_key}
+        self._url = '%s/actions/%s' % (client.base_url, request_type)
+
+    ## Post request
+    #  @return a requests.Response object
+    #  @param input request document file object
+    #  @param options e.g. {'pages': '1', 'noAnnot': True}
+    def post(self, input, **options):
+        self._data.update(options)
+        self._data['inputFile'] = input.name
+        return requests.post(self.url, data=self.data, files={'input': input})
+
+    @property
+    ## %Request data property, set by #post
     def data(self): return self._data
     @property
-    ## request URL property
+    ## %Request URL property
     def url(self): return self._url
 
 
 class ImageRequest(Request):
-    ## @param api_key from <a href="http://3scale.net/">TODO: fix link</a>
-    def __init__(self, api_key, version=Request.VERSION, base_url=Request.URL):
-        request_type = 'image'
-        Request.__init__(self, request_type, api_key, version, base_url)
+    def __init__(self, client):
+        Request.__init__(self, client, 'image')
+
+    ## Post request
+    #  @return a requests.Response object
+    #  @param input request document file object
+    #  @param output_form output graphic format, e.g. 'TIF'
+    #  @param options e.g. {'pages': '1', 'noAnnot': True}
+    #
+    #  The following options are interpreted as bool (default=False):
+    #  * OPP
+    #  * asPrinted
+    #  * blackIsOne
+    #  * multiPage
+    #  * noAnnot
+    #  * noCMM
+    #  * noEnhanceThinLines
+    #  * reverse
+    #
+    #  %ImageRequest's 'height' and 'width' options specify the image's
+    #  dimensions, replacing [PDF2IMG](../PDF2IMG.pdf)'s pixelcount.
+    #  See PDF2IMG for more information about the remaining options:
+    #  * BPC
+    #  * colorModel
+    #  * compression
+    #  * fontList
+    #  * jpegQuality
+    #  * maxBandMem
+    #  * output
+    #  * pages
+    #  * password
+    #  * pdfRegion
+    #  * resolution
+    #  * smoothing
+    def post(self, input, output_form, **options):
+        self._data['outputForm'] = output_form
+        return Request.post(self, input, **options)
 
